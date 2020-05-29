46e1590f7598f7350c6c4a2478f14c4c9a2f6138
Thiago Giammattey <giammattey@gmail.com>
2020-05-29 13:52:34 -0300

    create highlight component with content

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const Highlight = ({ children, color }) => (
+  <>
+    <span className={`relative highlight highlight-${color}`}>
+      <span className="relative z-2">{children}</span>
+    </span>
+  </>
+);
+
+const Intro = () => (
+  <div className="m-auto-ns f4 f3-m f2-l tc w-80-l normal">
+    <p className="mb3 mb4-ns">
+      <Highlight color="aqua">Lost in Tokyo</Highlight> is a directory of fun
+      places to see, play in and <Highlight color="yellow">explore</Highlight>,
+      in <Highlight color="blue">Tokyo</Highlight>, Japan.
+    </p>
+    <p>
+      From <Highlight color="blue">museums</Highlight> and
+      <Highlight color="blue">galleries</Highlight>, to
+      <Highlight color="pink">robot restaurants</Highlight> and
+      <Highlight color="pink">kitten cafes</Highlight>, Tokyo is the gift that
+      keeps on giving. <Highlight color="yellow">Dattebayo!</Highlight>
+    </p>
+  </div>
+);
+
+const App = () => (
+  <div>
+    <div className="min-vh-100 ph4 flex flex-column">
+      {/* our navigation component */}
+      <Intro></Intro>
+    </div>
+    <div className="flex flex-wrap container">
+      {/* our attractions list component */}
+    </div>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
